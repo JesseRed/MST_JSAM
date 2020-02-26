878ce0f0265044d880c91b41121e7880848c7ce2
Jesse Red <carsten.klingner@gmail.com>
2020-02-26 19:29:31 +0100

    WORKING network

--- analyse_csvs/network.py
+++ analyse_csvs/network.py
@@ -8,19 +8,17 @@ import statistics
 import networkx
 import time
 from mst import MST
+import random
 
 class Network():
     def __init__(self, ipi, coupling_parameter = 0.03,  resolution_parameter = 0.9):
+        # whether in the adaptation process it will be tried to set the
+        # node in the next trial to the same node of the previous
+        self.is_adapt_communities_across_trials = False 
+        
         self.ipi = ipi
         self.coupling_parameter = coupling_parameter
         self.resolution_parameter = resolution_parameter    
-        
-        self.estimate_chunks()
-        
-        
-
-    def estimate_chunks(self):
-
         ipi = self.ipi
         self.ipi_arr = self.convert_to_array2D(self.ipi)
         #print(self.ipi_arr.shape)
@@ -30,120 +28,273 @@ class Network():
         self.ipi_norm_arr = self.convert_to_array2D(self.ipi_norm)
         self.A = self.get_adjacency_matrix(self.ipi_norm_arr) 
         self.C = self.get_inter_slice_coupling(self.ipi_norm_arr, self.coupling_parameter)
-        
-        self.g = self.initialize_g() # jedes node ist seine eigene community
-        start = time.time()
-        self.Qms = self.get_Qms(self.g, 1, 1)
-        print(f"Qms execution time = {time.time()-start:.3} s")
+        self.delta_intra_slice = self.get_delta_intra_slice(self.A)
+        self.delta_inter_slice = self.get_delta_inter_slice(self.C)
 
-        self.adapt_communities()
+        self.c = np.sum(self.C,axis=2)
+        self.k = np.sum(self.A,axis=0)
+        self.kappa = self.k + self.c
+        self.my2 = sum(sum(self.kappa))
+        self.m = np.sum(self.k, axis=0)
+        self.gamma = np.zeros((self.A.shape[2]))+self.resolution_parameter
+
+
+        #Randomize
+        self.ipi_norm_arr_shuffled = self.shuffle(self.ipi_norm_arr)
+        self.P_list = []
+        for i in range(100):
+            self.ipi_norm_arr_shuffled = self.shuffle(self.ipi_norm_arr)
+            self.P_list.append(self.get_adjacency_matrix(self.ipi_norm_arr_shuffled))
+            
+        self.P = self.P_list[0]
+
         
+        # the partition optimized by Qmulti_trial
+        #self.g = self.estimate_chunks()
+        
+        #self.phi = self.estimate_chunk_magnitudes(self.g)
+        
+    def shuffle(self, ipi):
+        for i in range(ipi.shape[0]):
+            random.shuffle(ipi[i,:])
+        return ipi
+        
+    def estimate_chunk_magnitudes(self, g):
+        '''
+        Chunk magnitude (ϕ) is defined as 1/Qsingle–trial. Low values of ϕ correspond to trials with
+        greater segmentation, which are computationally easier to split into chunks and high values
+        of ϕ correspond to trials with greater chunk concatenation, which contain chunks that are
+        more difficult to computationally isolate. We normalized the values of ϕ across correct trials
+        for each frequent sequence: (Wymbs et al. 2013)
+        
+        '''
+        print("estimating chunk magnitudes")
+        #print(g.T)
+        P = self.P
+        phi = []
+        for slice in range(g.shape[1]):
+            Q_st = self.estimate_Q_st(g[:,slice],self.A[:,:,slice], P[:,:,slice])
+            
+            phi.append(1/Q_st)
+        #normalization
+        m = sum(phi)/len(phi)
+        for i in phi:
+            i = (i-m)/m
+        self.phi = phi
+        return phi
+
+
+    def estimate_Q_st(self, trial, W, P):
+        summe = 0
+        for i in range(W.shape[0]):
+            for j in range(W.shape[1]):
+                summe = summe +(W[i,j]-P[i,j])*self.get_delta_2d(trial[i],trial[j])
+        return summe
+    
+    def get_delta_2d(self,i,j):
+        ret = (1 if i==j else 0)
+        return ret
+    
+    def estimate_chunks(self, is_random=False):
+        ''' A ist die Adjacency matrix anhand der die Berechnung der chunks
+            erfolgt. Diese kann dann auch durch die Random Adjacency matrix 
+            ersetzt werden.
+        '''
+        if not is_random:
+            A = self.A # setzte die normale Adjecency matrix
+        else:
+            # Shuffle the data
+            ipi_norm_arr_shuffled = self.shuffle(self.ipi_norm_arr)
+            A = self.get_adjacency_matrix(ipi_norm_arr_shuffled)
+            
+        g = self.initialize_g() # jedes node ist seine eigene community
         start = time.time()
-        self.Qms = self.get_Qms(self.g, 1,1)
+        Qms_old = self.get_Qms_opt(g, A)
         print(f"Qms execution time = {time.time()-start:.3} s")
-                
+        idx = 0
+        while True:
+            idx +=1
+            start = time.time()
+            g = self.adapt_communities(g,A)
+            Qms = self.get_Qms_opt(g, A)
+            if Qms<=Qms_old:
+                break
+            else:
+                Qms_old=Qms
+#            print(g.T)
+            print(f"------------------------------------------")
+            print(f"----------Durchlauf = {idx} --------------")
+            print(f"postadapt ... unique elements in g: {np.unique(g).shape[0]}")
+            print(f"adapt_communities execution time = {time.time()-start:.3} s")                    
+            print(f"durchlauf={idx} mit Qms = {Qms}")
+        Qms = self.get_Qms_opt(g, A)
+        print(f"Final Qms = {Qms}")
+        return (g, Qms)
         #self.w = self.get_simple_weights(self.ipi_norm_arr)
         #self.w_norm = self.get_normalized_weigths(self.ipi_norm)
         
-    def adapt_communities(self):
+    def adapt_communities(self,g, A):
         # Anpassung von g entsprechend dem Algorithmus von Blondel 2008
         # g wird durchgegangen und g[i,s] wird in seinerm community
         # ersetzt durch g[i-1,s] und g[i+1,s]
         # wenn sich eine Verbesserung einstellt dann wird die neue
         # Zuordnung beibehalten
-        g = self.g.copy()
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print(f"praeadapt ... unique elements in g: {np.unique(g).shape[0]}")
-        print(f"g.shape = {g.shape}")
+
+#        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+ 
         index = 0
         for s in range(g.shape[1]):
             for i in range(g.shape[0]):
                 index+=1
-                print(f"{i} ... unique elements in g: {np.unique(g).shape[0]}")
-                print(f"{g[:,0]}")
-                print(f"g[i,s]= {g[i,s]}")
-                original_entry = g[i,s]
-                dq1 = -1
-                dq2 = -1
-                g_tmp= g.copy()
-                
+                #print(f"{i} ... unique elements in g: {np.unique(g).shape[0]}")
+                #print(f"{g[:,0]}")
+                #print(f"g[i,s]= {g[i,s]}")
+                community_org = g[i,s]
+                community_links = -1
+                community_rechts = -1
+                community_unten = -1
+                q_org = self.get_Qms_opt(g,A)
+                q_links = -999
+                q_rechts = -999
+                q_unten = -999
+                #print(f"original Q0 = {q_org}")
                 if i>0:
                     if not g[i,s]==g[i-1,s]:
-                        g_tmp[i,s] = g[i-1,s]
-                        dq1 = self.get_Qms(g_tmp,i,s)
-#                        dq1 = self.get_delta_q(g,i,s)
-                if i<9:
+                        g[i,s] = g[i-1,s]
+                        q_links = self.get_Qms_opt(g,A)
+                        community_links = g[i,s]
+
+                if i<g.shape[0]-1:
                     if not g[i,s]==g[i+1,s]:
-                        g_tmp[i,s] = g[i+1,s]
-#                        dq2 = self.get_delta_q(g,i,s)
-                        dq2 = self.get_Qms(g_tmp,i,s)
-                # setze nun g[i,s] auf den neuen Wert
-                if dq1>=dq2 and abs(dq1)>=0:
-                    g[i,s]=g[i-1,s]
-                elif dq1<dq2 and abs(dq2)>=0:
-                    g[i,s]=g[i+1,s]
-                else:
-                    g[i,s]=original_entry
-                if index>5:
-                    break
-            break
-        print(f"postadapt ... unique elements in g: {np.unique(g).shape[0]}")
+                        g[i,s] = g[i+1,s]
+                        q_rechts = self.get_Qms_opt(g,A)
+                        community_rechts = g[i,s]
+
+                #! nach unten oder nach oben?
+                if self.is_adapt_communities_across_trials: 
+                    if s<g.shape[1]-1:
+                        if not g[i,s]==g[i,s+1]:
+                            g[i,s] = g[i,s+1]
+                            q_unten = self.get_Qms_opt(g,A)
+                            community_unten = g[i,s]
+
+                results = [q_org, q_links, q_rechts, q_unten]
+                #print(f"adapt communites results = {results}")
+                g_communities = [community_org, community_links, community_rechts, community_unten]
+                # ist Q wirklich immer positiv wenn es besser wird????
+                g[i,s] = g_communities[results.index(max(results))] # die community zuordnung des maximums
+   
+                #self.g = g
+
+        
+
+
+        return g
+        #print(f"postadapt ... unique elements in g: {np.unique(g).shape[0]}")
                 
                 
 
     def initialize_g(self):
         # initialization with every node is a community
-        print(self.A.shape)
         g = np.arange(self.A.shape[0]*self.A.shape[2]).reshape(self.A.shape[0],self.A.shape[2])
-        print(g)
-        print(g.shape)
+        #g = np.ones((self.A.shape[0],self.A.shape[2]),dtype=int)
         return g
     
-    def get_Qms(self, g, i, s):
+    def get_Qms_opt(self, g, A):
+
+
+
+        summe =  0
+        #start= time.time()
+        for i in range(A.shape[0]):
+            j_start = (i-1 if i>0 else i)
+            j_stop = (i+2 if i<A.shape[0]-1 else i+1)
+            for j in range(j_start,j_stop):
+                for s in range(A.shape[2]):
+                    r_start = (s-1 if s>0 else s)
+                    r_stop = (s+2 if s<A.shape[2]-1 else s+1)
+                    for r in range(r_start,r_stop):
+                        x = A[i,j,s] - ((self.gamma[s]*self.k[i,s]*self.k[j,s])/(2*self.m[s]))
+                        x = x * self.delta_inter_slice[s,r]
+                        x = x + self.delta_intra_slice[i,j] * self.C[j,s,r]
+                        x = x * self.get_delta_community(g[i,s],g[j,r])
+                        summe = summe + x 
+                        #if self.get_delta_community(g[i,s],g[j,r])>0:
+                            #print(self.get_delta_community(g[i,s],g[j,r]))
+                        #if i==0:
+                        #    print(f"[i,j,s,r] = [{i},{j},{s},{r}]")
+#                        if i==0 and j==1 and s==0 and r==0:
+#                            print(f"A[{i},{j},{s}]={A[i,j,s]}")
+#                            xxx = ((gamma[s]*k[i,s]*k[j,s])/(2*m[s]))
+#                            print(f"gamma[s]*k[i,s]*k[j,s])/(2*m[s]) = {xxx}")
+#                            print(f"delta_inter_slice[{s},{r}]={delta_inter_slice[s,r]}")
+#                            print(f"delta_intra_slice[{i},{j}]={delta_intra_slice[i,j]}")
+#                            print(f"C[{j},{s},{r}]={C[j,s,r]}]")
+#                            print(f"self.get_delta_community(g[{i},{s}],g[{j},{r}]) = {self.get_delta_community(g[i,s],g[j,r])}")
+#        
+        Qms = 1/self.my2 * summe
+        
+        #print(f"estimation time = {time.time()-start} s")
+        return Qms
+    
+    def get_Qms_optx(self, g, i, s):
         A = self.A
         C = self.C
-        print(f"A.shape = {A.shape}")
-        print(f"C.shape = {C.shape}")
-        print("C")
-        print(C)
+        print(f"----------------------")
+        print(f"start estimate Qms")
+        print(f"{g[:,0]}")
+        print(f"{g[:,1]}")
+        #print(f"C.shape = {C.shape}")
+        #print("C")
+        #print(C)
         c = np.sum(C,axis=2)
-        print("c")
-        print(c)
+        #print("c")
+        #print(c)
         k = np.sum(A,axis=0)
-        print("k")
-        print(k)
+        #print("k")
+        #print(k)
         kappa = k + c
-        print("kappa")
-        print(kappa)
+        #print("kappa")
+        #print(kappa)
         my2 = sum(sum(kappa))
-        print("my2")
-        print(my2)
+#        print("my2")
+#        print(my2)
         m = np.sum(k, axis=0)
-        print("m")
-        print(m)
+#        print("m")
+#        print(m)
         gamma = np.zeros((A.shape[2]))+self.resolution_parameter
-        print("gamma")
-        print(gamma)
         delta_intra_slice = self.get_delta_intra_slice(A)
         delta_inter_slice = self.get_delta_inter_slice(C)
         
         summe =  0
         delta = 0
+        start= time.time()
         for i in range(A.shape[0]):
             for j in range(A.shape[1]):
                 for s in range(A.shape[2]):
                     for r in range(C.shape[2]):
-                        x = A[i,j,s] - gamma[s]*k[i,s]*k[j,s]/(2*m[s])
+                        x = A[i,j,s] - ((gamma[s]*k[i,s]*k[j,s])/(2*m[s]))
                         x = x * delta_inter_slice[s,r]
                         x = x + delta_intra_slice[i,j] * C[j,s,r]
                         x = x * self.get_delta_community(g[i,s],g[j,r])
                         summe = summe + x 
-
+                        #if self.get_delta_community(g[i,s],g[j,r])>0:
+                            #print(self.get_delta_community(g[i,s],g[j,r]))
+                        if i==0 and j==1 and s==0 and r==0:
+                            print(f"A[{i},{j},{s}]={A[i,j,s]}")
+                            xxx = ((gamma[s]*k[i,s]*k[j,s])/(2*m[s]))
+                            print(f"gamma[s]*k[i,s]*k[j,s])/(2*m[s]) = {xxx}")
+                            print(f"delta_inter_slice[{s},{r}]={delta_inter_slice[s,r]}")
+                            print(f"delta_intra_slice[{i},{j}]={delta_intra_slice[i,j]}")
+                            print(f"C[{j},{s},{r}={C[j,s,r]}]")
+                            print(f"self.get_delta_community(g[{i},{s}],g[{j},{r}]) = {self.get_delta_community(g[i,s],g[j,r])}")
         print("summe")
         print(summe)
+        
 
         Qms = 1/my2 * summe
         print(f"Qms = {Qms}")
+        print(f"estimation time = {time.time()-start} s")
         return Qms
     
     def get_delta_community(self, g1,g2):
@@ -165,19 +316,23 @@ class Network():
     def get_delta_inter_slice(self, C):
         delta_inter_slice = np.zeros((C.shape[1],C.shape[2]))
         for s in range(delta_inter_slice.shape[0]-1):
+            delta_inter_slice[s,s]=1
+            delta_inter_slice[s+1,s+1]=1
             delta_inter_slice[s,s+1]=1
             delta_inter_slice[s+1,s]=1
         return delta_inter_slice
 
     def get_inter_slice_coupling(self, ipi, coupling_parameter):
         C = np.zeros((ipi.shape[1],ipi.shape[0],ipi.shape[0]))        
-        print(f"C.shape = {C.shape}")
+        #print(f"C.shape = {C.shape}")
         for j in range(C.shape[0]):
             for s in range(C.shape[1]-1):
                 C[j,s,s+1] = coupling_parameter                    
-                C[j,s+1,s] = coupling_parameter
+                C[j,s+1,s] = coupling_parameter # raus weil es geht nur vorwaerts in der Zeit
         return C                    
         
+     
+    
     def get_adjacency_matrix(self, ipi):
         # ipi ist eine N x M  Array  
         # i element N ist die slice 
@@ -453,10 +608,58 @@ if __name__ == '__main__':
     mst = MST(filename)
 
     net = Network(mst.ipi_cor, coupling_parameter = 0.03,  resolution_parameter = 0.9)
+    #g = net.g
+    g_real,q_real = net.estimate_chunks(is_random = False)
+    phi_real = net.estimate_chunk_magnitudes(g_real)
+    g_fake_list = []
+    q_fake_list = []
+    phi_fake_list = []
+    for i in range(10):
+        g, q = net.estimate_chunks(is_random = True)
+        phi = net.estimate_chunk_magnitudes(g)
+        g_fake_list.append(g)
+        q_fake_list.append(q)
+        phi_fake_list.append(phi)
+#    g = np.load("g_arr.npy")
+    
+    #Qms_list = net.Qms_list
+    ipi = net.ipi_norm_arr
+    #the partition optimized by Qmulti_trial
+    #g = self.estimate_chunks()
+        
     
     #w_norm = mst.w_norm
     #print(type(mst.ipi_cor))
     #print(len(mst.ipi_cor))
 
-
-
+#model.fit(ta.reshape(-1,1),phia)
+#Out[36]: LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
+#
+#model.intercept_
+#Out[37]: 0.85201842328977
+#
+#model.coef_
+#Out[38]: array([-0.04204932])
+#
+#from numpy.polynomial.polynomial import polyfit
+#
+#b, m = polyfit(ta, phia, 1)
+#
+#
+#x = ta
+#
+#y = phi
+#
+#plt.plot(x, y, '.')
+#plt.plot(x, b + m * x, '-')
+#plt.show()
+#t = list(range(63))
+#
+#ta = np.asarray(t, dtype=int)
+#
+#phia = np.asarray(phi, dtype=np.float32)
+#with open("results_unten.txt","wb") as fp:
+#    pickle.dump(q_real,fp)
+#    pickle.dump(q_fake_list, fp)
+#    pickle.dump(g_real,fp)
+#    pickle.dump(g_fake_list,fp)
