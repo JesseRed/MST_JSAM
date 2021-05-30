35e2cc185b88122f99d1736e7aaeb6c6fcb7f35d
jessered <carsten.klingner@gmail.com>
2021-05-31 01:05:36 +0200

    leiden test works in temp3.py

--- analyse_csvs/tmp3.py
+++ analyse_csvs/tmp3.py
@@ -7,6 +7,7 @@ from seq import SEQ
 from srtt import SRTT
 from experiment import Experiment
 from igraph import Graph
+import leidenalg as la
 
 filename = "./Data_Rogens/SEQ_Sim/34_SEQsimpleFRA1fertig.csv"
 path_outputfiles="./tmp"
@@ -35,31 +36,46 @@ ipi_cor = exp_est.make2dlist_to_2darray(subj_exp.cor_ipi_lplsln[subj_exp.paradig
 #        [-781, 2578, 1607, 1510,  700, 1007, 2409, 3002]])
 
 edges = [[0,1],[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
-gl = list()
-for i in range(len(ipi_cor)):
-    g = Graph(n=8, edges = edges, directed = True)
-    g.es['weight'] = ipi_cor[i]
-    gl.append(g)
-    
-edges = [[0,1],[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
 G_1 = Graph(n=8, edges = edges, directed = True)
 G_1.es['weight'] = ipi_cor[1]
+G_1.vs['id']= [0,1,2,3,4,5,6,7]
 G_2 = Graph(n=8, edges = edges, directed = True)
 G_2.es['weight'] = ipi_cor[2]
+G_2.vs['id']= [0,1,2,3,4,5,6,7]
 G_3 = Graph(n=8, edges = edges, directed = True)
 G_3.es['weight'] = ipi_cor[3]
-import leidenalg as la
-optimiser = la.Optimiser()
+G_3.vs['id']= [0,1,2,3,4,5,6,7]
+
 #https://leidenalg.readthedocs.io/en/latest/multiplex.html#temporal-community-detection
-G_1 = gl[0]
-G_2 = gl[1]
-G_3 = gl[2]
 G_coupling = Graph.Formula('1 -- 2 -- 3')
 G_coupling.es['weight'] = 0.1 # Interslice coupling strength
 G_coupling.vs['slice'] = [G_1, G_2, G_3]
-Gl_1 = la.slices_to_layers(G1)
+
+optimiser = la.Optimiser()
+
+
+
+
+layers, interslice_layer, G_full = la.slices_to_layers(G_coupling);
+
+partitions = [la.CPMVertexPartition(H, node_sizes='node_size',
+                                    weights='weight', resolution_parameter=0.9)
+                                    for H in layers];
+interslice_partition = la.CPMVertexPartition(interslice_layer, resolution_parameter=0,
+                                             node_sizes='node_size', weights='weight');
+diff = optimiser.optimise_partition_multiplex(partitions + [interslice_partition]);
+
 membership, improvement = la.find_partition_temporal(
                             [G_1, G_2, G_3],
                             la.CPMVertexPartition,
                             interslice_weight=0.1,
                             resolution_parameter=0.9)
+
+
+layers, interslice_layer, G_full = la.time_slices_to_layers([G_1, G_2, G_3],
+                                                            interslice_weight=0.1);
+partitions = [la.CPMVertexPartition(H, node_sizes='node_size',                                         weights='weight',
+                          resolution_parameter=0.9)
+                          for H in layers];
+interslice_partition =  la.CPMVertexPartition(interslice_layer,resolution_parameter=0, node_sizes='node_size', weights='weight')
+diff = optimiser.optimise_partition_multiplex(partitions + [interslice_partition]);
